3c9f3f56636d92a2edb1e7c97e152f740ee157e1
Parkle Lee <parkle.lee@xenyo.com>
2022-11-22 11:37:42 +0800

    Update ubuntu

--- ubuntu/startup.sh
+++ ubuntu/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-cp -R /tmp/.ssh /home/ubuntu/.ssh
+cp -R /mnt/.ssh /home/ubuntu/.ssh
 chown -R ubuntu:ubuntu /home/ubuntu/.ssh
 find /home/ubuntu/.ssh -type d -exec chmod 700 {} \;
 find /home/ubuntu/.ssh -type f -exec chmod 600 {} \;
