8848aefcfe8472c731a599c2619712954e1c51ed
Parkle Lee <parkle.lee@xenyo.com>
2022-11-24 11:22:04 +0800

    Templatize all containers

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const child_process = require('child_process');
-const fs = require('fs');
+const fs = require('fs-extra');
 const handlebars = require('handlebars');
-const mkdirp = require('mkdirp');
-const rimraf = require('rimraf');
+const walkSync = require('walk-sync');
 const yaml = require('js-yaml');
 
 const { images } = yaml.load(fs.readFileSync('images.yml', 'utf8'));
@@ -12,12 +11,12 @@ const command = process.argv[2];
 if (command === 'generate') {
   images.forEach(image => {
     const { src, dest, context } = image;
-    rimraf.sync(dest);
-    mkdirp.sync(dest);
-    fs.readdirSync(src).forEach(file => {
+    fs.removeSync(dest);
+    fs.mkdirpSync(dest);
+    walkSync(src, { directories: false }).forEach(file => {
       const template = handlebars.compile(fs.readFileSync(`${src}/${file}`, 'utf8'));
       const output = template(context);
-      fs.writeFileSync(`${dest}/${file}`, output);
+      fs.outputFileSync(`${dest}/${file}`, output);
     });
     console.log(`Generated ${dest}.`);
   });
